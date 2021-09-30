44b2872adaf3debd38ada88bc568e7086b4a6650
hmoller <hmoller.github@gmail.com>
2021-09-30 15:58:00 -0300

    agregar documentacion

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
-const jsonServer = require('json-server');
+require("dotenv").config();
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('./db/db.json');
-const db = require('./db/db.json');
-const { insert } = require('./db/handler');
+const router = jsonServer.router("./db/db.json");
+const db = require("./db/db.json");
+const { insert } = require("./db/handler");
 const middlewares = jsonServer.defaults();
-const checkJwt = require('./middlewares/auth');
-const handlerError = require('./middlewares/error');
-const delay = require('./middlewares/delay');
-const { createJwtToken } = require('./helper/auth');
+const checkJwt = require("./middlewares/auth");
+const handlerError = require("./middlewares/error");
+const delay = require("./middlewares/delay");
+const { createJwtToken } = require("./helper/auth");
 
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
@@ -16,21 +16,21 @@ server.use(handlerError);
 server.use(delay);
 server.use(checkJwt);
 
-server.post('/login', (req, res) => {
-  const email = req.body['email'];
-  const password = req.body['password'];
+server.post("/login", (req, res) => {
+  const email = req.body["email"];
+  const password = req.body["password"];
   const user = db.users.find((user) => user.email === email);
   if (!user) {
     res.status(401).json({
-      message: 'Invalid credentials',
-      errorCode: 'LOGIN_ERROR_INVALID_CREDENTIALS',
+      message: "Invalid credentials",
+      errorCode: "LOGIN_ERROR_INVALID_CREDENTIALS",
     });
     return;
   }
   if (password !== user.password) {
     res.status(401).json({
-      message: 'Invalid credentials',
-      errorCode: 'LOGIN_ERROR_INVALID_CREDENTIALS',
+      message: "Invalid credentials",
+      errorCode: "LOGIN_ERROR_INVALID_CREDENTIALS",
     });
     return;
   }
@@ -42,29 +42,30 @@ server.post('/login', (req, res) => {
   return;
 });
 
-server.get('/users/:id/favorites', (req, res) => {
-  const userId = req.params['id'];
+server.get("/users/:id([0-9]+)/favorites", (req, res) => {
+  const userId = req.params["id"];
   const favorites = db.favorites.find((favorite) => favorite.userId === userId);
   res.status(200).json({
     favorites,
   });
   return;
 });
-server.post('/users/:id([0-9]+)/favorites', (req, res) => {
-  const userId = req.params['id'];
+
+server.post("/users/:id([0-9]+)/favorites", (req, res) => {
+  const userId = req.params["id"];
   const { movieId } = req.body;
   if (!movieId) {
     res
       .status(422)
-      .json({ errorCode: 'INVALID_BODY', message: 'movieId is required' });
+      .json({ errorCode: "INVALID_BODY", message: "movieId is required" });
   }
   const favorite = db.favorites.find(
     (favorite) => favorite.userId === userId && favorite.movieId === movieId
   );
   if (favorite) {
     res.status(409).json({
-      errorCode: 'FAVORITE_ALREADY_REGISTER',
-      message: 'favorite is already register to user',
+      errorCode: "FAVORITE_ALREADY_REGISTERED",
+      message: "favorite is already registered to user",
     });
   }
   const newFavorite = {
@@ -72,33 +73,33 @@ server.post('/users/:id([0-9]+)/favorites', (req, res) => {
     userId,
     movieId,
   };
-  insert(router.db, 'favorites', newFavorite);
+  insert(router.db, "favorites", newFavorite);
   res.status(200).json({
     ...newFavorite,
   });
   return;
 });
 
-server.post('/register', (req, res) => {
+server.post("/register", (req, res) => {
   const { email, password, name } = req.body;
   if (!email) {
     res
       .status(422)
-      .json({ errorCode: 'INVALID_BODY', message: 'email is required' });
+      .json({ errorCode: "INVALID_BODY", message: "email is required" });
   } else if (!password) {
     res
       .status(422)
-      .json({ errorCode: 'INVALID_BODY', message: 'password is required' });
+      .json({ errorCode: "INVALID_BODY", message: "password is required" });
   } else if (!name) {
     res
       .status(422)
-      .json({ errorCode: 'INVALID_BODY', message: 'name is required' });
+      .json({ errorCode: "INVALID_BODY", message: "name is required" });
   }
   const user = db.users.find((user) => user.email === email);
   if (user) {
     res.status(409).json({
-      errorCode: 'USER_ALREADY_REGISTER',
-      message: 'User is already register',
+      errorCode: "USER_ALREADY_REGISTERED",
+      message: "User is already registered",
     });
   }
   const newUser = {
@@ -107,7 +108,7 @@ server.post('/register', (req, res) => {
     email,
     password,
   };
-  insert(router.db, 'users', newUser);
+  insert(router.db, "users", newUser);
   delete newUser.password;
   const token = createJwtToken(newUser);
   res.status(200).json({
@@ -118,5 +119,5 @@ server.post('/register', (req, res) => {
 
 server.use(router);
 server.listen(3000, () => {
-  console.log('JSON Server is running');
+  console.log("JSON Server is running");
 });
